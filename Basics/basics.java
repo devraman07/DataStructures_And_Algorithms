8ebbb94edcdd2c57175b5ac3710aae4e3bdc4198
devraman07 <devraman0007@gmail.com>
2026-06-12 11:41:27 +0530

    basics done

--- Basics/basics.java
+++ Basics/basics.java
@@ -3,29 +3,27 @@ import java.util.Scanner;
 public class basics {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        
-        // int a = sc.nextInt();
-        // int b = sc.nextInt();
-        //sum of a and b
-        // int sum =  a + b;
 
-        // System.out.println(sum);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         // sum of a and b
+         int sum = a + b;
 
-        // int product = a * b;
+        System.out.println(sum);
 
-        // System.out.println(product);
-    
-        // float radius = sc.nextFloat();
-        // float pi = 3.14f;
-        
+         int product = a * b;
 
-        // float area = pi * radius * radius;
+         System.out.println(product);
 
-        // System.out.print(area);
+        float radius = sc.nextFloat();
+        float pi = 3.14f;
 
+        float area = pi * radius * radius;
 
-        //calculating avarage of 3 nums 
-        // int a = sc.nextInt();
+        System.out.print(area);
+
+         // calculating avarage of 3 nums
+        // int  a = sc.nextInt();
         // float b = sc.nextFloat();
         // double c = sc.nextDouble();
 
@@ -33,19 +31,75 @@ public class basics {
 
         // System.out.println(avarage);
 
-
-        //creating a bill
+        // //creating a bill
         float pen = sc.nextFloat();
         float pencil = sc.nextFloat();
         float copy = sc.nextFloat();
 
-        
-
-       float bill =  (pen + pencil + copy);
+        float bill = (pen + pencil + copy);
 
         float gstBill = (bill * 18 /100) + bill;
 
         System.out.print(gstBill);
+
+        // conditional statements
+
+        // print largest between two
+
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+
+        // if(a > b) {
+        // System.out.println(a + " " + "is saying mein bada hu");
+        // } else {
+
+        // System.out.println(b + " " + "is saying mein bada hu");
+        // }
+
+        // check a number is even or odd
+
+        // int num = sc.nextInt();
+
+        // if(num % 2 == 0) {
+        // System.out.println(num + " " + "even hai");
+        // } else {
+        // System.out.println(num + " " + "odd hai");
+        // }
+
+        // Income tax on salary
+
+        int salary = sc.nextInt();
+
+        if (salary < 500000) {
+        System.out.println("No tax needed on" + " " + salary + " " + "rupees");
+        } else if (salary < 1000000) {
+        int tax = salary * 20 / 100;
+
+        System.out.println("rupees" + " " + tax + " " + " tax needed on your salary"
+        + " " + salary);
+        } else if (salary > 1000000) {
+        int tax = salary * 30 / 100;
+
+        System.out.println("rupees" + " " + tax + " " + " tax needed on your salary"
+        + " " + salary);
+
+        }
+
+        int marks = sc.nextInt();
+
+        String ifpASSoRnOT = marks >= 33 ? "pass" : "fail";
+
+        System.out.println(ifpASSoRnOT);
+        int n = sc.nextInt();
+        // int sum = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+        }
+
+        System.out.println(sum);
 
     }
 }
