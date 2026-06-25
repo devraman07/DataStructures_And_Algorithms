1e626a28c0c862e0d2d4bc97a8ddb1ae5817c5c9
devraman07 <devraman0007@gmail.com>
2026-06-25 17:30:08 +0530

    adv arrays done

--- advArrays/advarrays.java
+++ advArrays/advarrays.java
@@ -47,9 +47,94 @@ public class advarrays {
         System.out.println("The max sum is: " + maxSum);
     }
 
+    public static void Kadens(int arr[]) {
+        int cs = 0;
+        int ms = Integer.MIN_VALUE;
+
+        for(int i = 0; i < arr.length ; i++) {
+            cs = cs + arr[i];
+
+            if(cs < 0) {
+                cs = 0;
+            }
+
+            ms = Math.max(ms,cs);
+        }
+        System.out.println("Max sum is : " + ms);
+    }
+
+    //calculate trappedRainWater 
+    public static int TrappedRainWater(int height[]) {
+        int n = height.length;
+        //calculating left max boundry
+        int leftMax[] = new int[height.length];
+        leftMax[0] = height[0];
+        for(int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+        //calculating right max boundry
+        int rightMax [] = new int[height.length];
+        rightMax[n - 1] = height[n - 1];
+        for(int i = n - 2; i >= 0; i --) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+
+        int trappedWater = 0;
+        for(int  i = 0; i < n; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+
+            trappedWater += waterLevel - height[i];
+        }
+        return trappedWater;
+
+
+        
+    }
+
+    public static int BuyAndSellStocks(int prices[]) {
+        int n = prices.length;
+        int BuyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i < n; i++) {
+           if(BuyPrice < prices[i]) {
+            int profit = prices[i] -BuyPrice;
+             maxProfit = Math.max(maxProfit, profit);
+           } else {
+            BuyPrice = prices[i];
+           }
+        }
+
+        return maxProfit;
+    }
+
+    public static boolean checkDuplicates(int arr[]) {
+        int n = arr.length;
+            for(int i = 0; i < n - 1; i++) {
+                 for(int j = i + 1; j < n; j++) {
+                    if(arr[i] == arr[j]) {
+                     return true;
+                    }
+                 }
+            }
+            return false;
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 2, 4, 6, 8, 10 };
-        // MaxSubarraySum(arr);
-        MaxSubarraySumPrefix(arr);
+        int arr[] = {1, 2, 3, 4, 5, 6};
+        // // MaxSubarraySum(arr);
+        // // MaxSubarraySumPrefix(arr);
+        // Kadens(arr);
+
+        // int height [] = {
+        //     2, 4, 0, 6, 1, 3, 7
+        // };
+        // System.out.println(TrappedRainWater(height));
+
+    //     int prices[] = {
+    //         7, 1, 5, 3, 6, 4
+    //     };
+    //    System.out.println( "Max profit is : " + BuyAndSellStocks(prices));
+    System.out.println(checkDuplicates(arr));
     }
 }
